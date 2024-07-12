90c27904a0e9533d7318ea3bb4af766ed98a3f0b
josueramirez1 <josue67ramirez@gmail.com>
2024-07-12 01:53:57 -0700

    refactoring code to use arrays
    simpler to join numbers and add symbols to array for later use in functions

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
 const container = document.querySelector(".container");
 const display = document.querySelector(".input");
 const btn = [...document.querySelectorAll("button")];
+let arr = [];
+let tempArr = [];
 let expression = "";
 let secondValue = "";
 let symbol;
 
 document.addEventListener("click", (e) => {
   if (!e.target.matches("button")) return;
-  // if user clicks on clear button...
+  // if user clicks on clear button input field clears and expression clears
   if (e.target.matches(".clear")) {
     display.value = "";
+    expression = "";
     return;
   }
-
-  if (e.target.matches(".number")) {
+  // if user clicks on button it is displayed and numbers are added together as string
+  if (
+    (expression.includes(symbol) && e.target.matches(".number")) ||
+    e.target.matches(".number")
+  ) {
     let number = e.target.innerText;
+    tempArr.push(number);
     expression = expression + number;
     display.value = expression;
+    console.log(tempArr);
   }
-
+  // if user clicks on operator button an operator symbol is added to the expression string
   if (e.target.matches(".operator")) {
+    let exp = tempArr.join("");
     symbol = e.target.innerText;
+    tempArr = [];
+    arr.push(exp);
+    arr.push(symbol);
+    console.log(arr);
     expression = expression + symbol;
   }
-  if (expression.includes(symbol) && e.target.matches(".number")) {
-    display.value = "";
-    let number = e.target.innerText;
-    secondValue = secondValue + number;
-    display.value = secondValue;
-  }
+  // if the expression has an operator and the number button is then clicked again, the input field will reset and a second value will be used to display
+  //   if (expression.includes(symbol) && e.target.matches(".number")) {
+  //     display.value = "";
+  //     let number = e.target.innerText;
+  //     expression = expression + number;
+  //     display.value = expression;
+  //   }
 
-  let arr = expression.split(symbol);
-  console.log(arr);
+  //   let arr = expression.split(symbol);
+  //   console.log(symbol.name);
+  //   if (e.target.matches(".equals")) {
+  //     operate(arr[0], arr[1], symbol);
+  //   }
 });
 
 // Helper function
@@ -49,5 +66,7 @@ function divide(a, b) {
 }
 
 function operate(value1, value2, operation) {
+  parseInt(value1);
+  parseInt(value2);
   operation(value1, value2);
 }
