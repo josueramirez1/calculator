cfe97699530c11d88192e5bb21f4507ecfa58a60
josueramirez1 <josue67ramirez@gmail.com>
2024-07-12 00:47:43 -0700

    button inputs are now saved. after symbol is pressed, when clicking a new button, field is reset and number is added to field
    provides user feedback when using operator on a previous number to a new number

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 const container = document.querySelector(".container");
 const display = document.querySelector(".input");
 const btn = [...document.querySelectorAll("button")];
-let numberValue = [];
-console.log(btn);
+let expression = "";
+let secondValue = "";
+let symbol;
 
 document.addEventListener("click", (e) => {
   if (!e.target.matches("button")) return;
   // if user clicks on clear button...
   if (e.target.matches(".clear")) {
-    numberValue = [];
     display.value = "";
     return;
   }
-  // To save in an array
-  let btnValue = e.target.innerText;
-  numberValue.push(btnValue);
-  console.log(numberValue);
-  // To display on the input field
-  let displayedNumbers = numberValue.join("");
-  display.value = displayedNumbers;
+
+  if (e.target.matches(".number")) {
+    let number = e.target.innerText;
+    expression = expression + number;
+    display.value = expression;
+  }
+
+  if (e.target.matches(".operator")) {
+    symbol = e.target.innerText;
+    expression = expression + symbol;
+  }
+  if (expression.includes(symbol) && e.target.matches(".number")) {
+    display.value = "";
+    let number = e.target.innerText;
+    secondValue = secondValue + number;
+    display.value = secondValue;
+  }
+
+  let arr = expression.split(symbol);
+  console.log(arr);
 });
 
 // Helper function
