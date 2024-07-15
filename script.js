3b38931ceba16b44103fc77de7e5caf5bbd92b72
josueramirez1 <josue67ramirez@gmail.com>
2024-07-15 14:37:15 -0700

    changing to parseFloat and adding decimal support. Disabling decimal
    Provides user the opportunity to calculate using decimal numbers

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 // Selectors
 const container = document.querySelector(".container");
 const display = document.querySelector(".input");
-const numBtn = [...document.querySelectorAll(".number")];
 const operatorBtn = [...document.querySelectorAll(".operator")];
+const period = document.querySelector(".period");
 // Global variables
 let arr = [];
-
-// let b;
 
 container.addEventListener("click", (e) => {
   if (!e.target.matches("button")) return;
@@ -14,33 +12,28 @@ container.addEventListener("click", (e) => {
   if (e.target.matches(".clear")) {
     display.value = "";
     arr = [];
-    // numBtn.forEach((num) => {
-    //   num.classList.remove("number2");
-    //   num.classList.add("number");
-    // });
     return;
   }
   if (e.target.matches(".number")) {
     // if operator was clicked before, display will be reset and attribute will be removed.
     clearDisplayAfterOp();
+    operatorBtn.forEach((btn) => (btn.disabled = false));
+    period.disabled = false;
 
-    // display.value = "";
     // Select number
     let number = e.target.innerText;
     // expression variable is given the number
-    display.value = display.value + number;
+    if (display.value === "0") {
+      display.value = number;
+    } else display.value = display.value + number;
     // expression is displayed in input field
   }
-  // if (e.target.matches(".number2")) {
-  //   // if operator was clicked before, display will be reset and attribute will be removed.
-  //   clearDisplayAfterOp();
-  //   // Select number
-  //   let number = e.target.innerText;
-  //   // expression variable is given the number
-  //   display.value = display.value + number;
-  //   // expression is displayed in input field
-  //   b = display.value;
-  // }
+
+  if (e.target.matches(".period")) {
+    let period = e.target.innerText;
+    display.value = display.value + period;
+    period.disabled = true;
+  }
 
   if (e.target.matches(".operator")) {
     let equals = document.querySelector(".equals");
@@ -49,16 +42,14 @@ container.addEventListener("click", (e) => {
     }
     // An attribute is set to the html to reference if user decides to click on number again
     e.target.setAttribute("data-click", true);
-    arr.push(parseInt(display.value));
+    arr.push(parseFloat(display.value));
     arr.push(e.target.innerText);
 
     // Run operate function if two conditions have been satisfied
     if (arr.length > 3) {
       beginToCalculate();
       arr.splice(0, 3);
-      console.log(arr);
-      arr.unshift(parseInt(display.value));
-      console.log(arr);
+      arr.unshift(parseFloat(display.value));
     }
   }
 
@@ -67,21 +58,23 @@ container.addEventListener("click", (e) => {
     beginToCalculate();
     arr = [];
   }
-  console.log(arr);
 });
 
 // Helper function
 function beginToCalculate() {
-  let value1 = arr[0];
-  let operator = arr[1];
-  let value2 = parseInt(display.value);
+  let [value1, operator] = arr;
+  // let value1 = arr[0];
+  // let operator = arr[1];
+  let value2 = parseFloat(display.value);
   if (arr[1] === "*") operator = multiply;
   if (arr[1] === "/") operator = divide;
   if (arr[1] === "+") operator = add;
   if (arr[1] === "-") operator = subtract;
 
   let result = operate(value1, value2, operator);
-  display.value = result;
+  if (result === "Nice try") {
+    display.value = result;
+  } else display.value = Math.round(result * 10) / 10;
 }
 
 function clearDisplayAfterOp() {
@@ -106,5 +99,7 @@ function divide(a, b) {
 }
 
 function operate(value1, value2, operation) {
-  return operation(value1, value2);
+  if (value2 === 0) {
+    return "Nice try";
+  } else return operation(value1, value2);
 }
